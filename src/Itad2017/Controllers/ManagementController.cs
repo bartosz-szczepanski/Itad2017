c33a5f2e9b14482e8a185a460d5c8384b9d773b3
Bartosz Szczepański-Bernaś <bartosz.bernas@euvic.pl>
2017-01-19 11:48:13 +0100

    still doing code refactoring

--- src/Itad2017/Controllers/ManagementController.cs
+++ src/Itad2017/Controllers/ManagementController.cs
@@ -6,39 +6,39 @@ using System.Threading.Tasks;
 
 namespace Itad2017.Controllers
 {
-    public class ManagementController
+    public class ManagementController : Controller
     {
         public ManagementController()
         {
 
         }
 
-        public IActionResult Index() // login to account
+        public ActionResult Index() // login to account
         {
             return View();
         }
 
-        public IActionResult LogOut() // logout from account
+        public ActionResult LogOut() // logout from account
         {
             return View();
         }
 
-        public IActionResult Helpers() // list of people assigned to help and prelegents
+        public ActionResult Helpers() // list of people assigned to help and prelegents
         {
             return View();
         }
 
-        public IActionResult Participants() // page with list of signed people and managing them
+        public ActionResult Participants() // page with list of signed people and managing them
         {
             return View();
         }
 
-        public IActionResult Workshops() // list of all workshops with number of signed people
+        public ActionResult Workshops() // list of all workshops with number of signed people
         {
             return View();
         }
 
-        public IActionResult Workshop() // Details and general informations about one workshop and list of people assigned to this workshop
+        public ActionResult Workshop() // Details and general informations about one workshop and list of people assigned to this workshop
         {
             return View();
         }
--- src/Itad2017/Controllers/RegistrationController.cs
+++ src/Itad2017/Controllers/RegistrationController.cs
@@ -1,34 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Itad2017.Data;
+using Itad2017.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Itad2017.Models;
 
 namespace Itad2017.Controllers
 {
-    public class RegistrationController
+    public class RegistrationController : Controller
     {
         public RegistrationController()
         {
 
         }
 
-        public IActionResult Index() // page for registration
+        public ActionResult Index() // page for registration
         {
             return View();
         }
 
-        public IActionResult Success() // landing after registration
+        public ActionResult Success() // landing after registration
         {
             return View();
         }
 
-        public IActionResult Confirm() // page for email confirm
+        public ActionResult Confirm() // page for email confirm
         {
             return View();
         }
 
-        public IActionResult Finish() // landing after email confirmation
+        public ActionResult Finish() // landing after email confirmation
         {
             return View();
         }
